10407ce8a90b1280bc90703760b2423f1a4fb5c9
Elliot <ellioteweb@gmail.com>
2022-10-31 20:20:41 +1100

    packages

--- Assets/Scripts/Audio/AudioHandler.cs
+++ Assets/Scripts/Audio/AudioHandler.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Abstracts;
 using Events;
 using ScriptableObjects;
 using UnityEngine;
@@ -14,35 +13,21 @@ namespace Audio
         [SerializeField] private float clipPlayWaitTime = 0.5f;
         private bool _isAvailable = true;
 
-        private void Start()
-        {
-            AddConstructionType();
-            _clips = this.constructionMaterial.audioClips;
-        }
-
         private void OnCollisionEnter2D(Collision2D col)
         {
-            if (_isAvailable == false)
+            if (!_isAvailable == false)
                 return;
             
             StartCoroutine(StartCooldown());
             AudioClip randomClip = GetRandomClip(_clips);
             GameEvents.OnAudioCollisionEvent?.Invoke(randomClip);
         }
-
+        
         AudioClip GetRandomClip(List<AudioClip> listOfClips)
         {
             AudioClip clip = listOfClips[Random.Range(0, listOfClips.Count)];
 //            Debug.Log("We got a random clip to play " + clip);
             return clip;
-        }
-
-        void AddConstructionType()
-        {
-            if (constructionMaterial == null && GetComponent<Receptacle>())
-            {
-                constructionMaterial = GetComponent<Receptacle>().ConstructionMaterial;
-            }
         }
 
         IEnumerator StartCooldown()
