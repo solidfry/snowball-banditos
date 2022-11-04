8e7ea8d82ed95f4c280afad075df03a94035063d
Elliot <ellioteweb@gmail.com>
2022-11-04 15:12:49 +1100

    recharge zone and charges for snowballs

--- Assets/Scripts/Player/Attack.cs
+++ Assets/Scripts/Player/Attack.cs
@@ -1,13 +1,22 @@
+using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Player
 {
     public class Attack : MonoBehaviour
     {
-        bool _isAttacking;
+        [Header("Attack Information")]
+        [SerializeField] bool _isAttacking;
         [SerializeField] float forceFactor;
         [SerializeField] GameObject attackPrefab;
         [SerializeField] Transform origin;
+        [SerializeField] private float cooldownTime = 0.5f;
+        
+        [Header("Projectile Charges")]
+        [SerializeField] int charges = 5, maxCharges = 5;
+        [SerializeField] private string rechargeTargetTag = "RechargeZone";
+        [SerializeField] GameObject rechargePrefab;
         Camera _cam;
 
         void Start()
@@ -17,11 +26,36 @@ namespace Player
 
         void OnAttack()
         {
-            var throwObject = Instantiate(attackPrefab, origin.position, Quaternion.identity);
-            throwObject.GetComponent<Rigidbody>().AddForce(_cam.transform.forward * forceFactor);
-            // isAttacking = true;
-            Debug.Log("Attacking");
+            if(charges > 0 && !_isAttacking)
+            {
+                charges--;
+                StartCoroutine(CoolDown());
+                var throwObject = Instantiate(attackPrefab, origin.position, Quaternion.identity);
+                throwObject.GetComponent<Rigidbody>().AddForce(_cam.transform.forward * forceFactor);
+                // isAttacking = true;
+                Debug.Log("Attacking");
+            }
         }
 
+        IEnumerator CoolDown()
+        {
+            _isAttacking = true;
+            yield return new WaitForSeconds(cooldownTime);
+            _isAttacking = false;
+        }
+
+        void ResetCharges()
+        {
+            charges = maxCharges;
+            Instantiate(rechargePrefab, transform.position, quaternion.identity);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag(rechargeTargetTag))
+            {
+                ResetCharges();
+            }
+        }
     }
 }
